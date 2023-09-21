a4eed9271af0c064e9bdd7c9035b50f2f5c9c326
rishabh dewangan <rishabh@nickelfox.com>
2023-09-21 20:02:50 +0530

    feat(Regenerate Invite): Regernate Invite and dropdown changes

--- apps/invitation/admin.py
+++ apps/invitation/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import reverse
+
+from apps.user.models import User
 from .models import Invitation
 from apps.role.models import AccessRole
 from common.constants import Invite_type
@@ -15,8 +17,30 @@ class InvitationAdmin(admin.ModelAdmin):
     list_display = ("fullname", "email", "role", "invite_link", "is_active")
     list_filter = ("is_active",)
     search_fields = ("fullname", "email")
-    readonly_fields = ("invite_link",)
+    readonly_fields = ("invite_link", "is_active")
     fields = ("fullname", "email", "role", "invite_link", "is_active")
+
+    def regenerate_invitation(self, request, queryset):
+        for invitation in queryset:
+            # Generate a new invite_link and send it to the email
+            user_obj = User.objects.filter(email=invitation.email)
+            if user_obj:
+                user_obj.delete()
+            invitation.is_active = True
+            invitation.save()
+            # Send the new invite to the email (you should implement this part)
+
+    regenerate_invitation.short_description = "Regenerate Invitations"
+
+    actions = [regenerate_invitation]
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        user = request.user
+        if not user.is_super_user:
+            if not module_perm("invitation", user, "regenerate"):
+                actions.pop("regenerate_invitation", None)
+        return actions
 
     def save_model(self, request, obj, form, change):
         # Save the object initially to generate obj.id
@@ -29,7 +53,26 @@ class InvitationAdmin(admin.ModelAdmin):
             rev_url = "user:accept_invitation"
             invite_link = f"{HOST_URL}{reverse(rev_url, args=[str(new_uuid)])}"
             obj.invite_link = invite_link
+            obj.company_id = None
             obj.save()
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "role":
+            obj = request._obj_
+            if obj:
+                kwargs["queryset"] = AccessRole.objects.filter(
+                    company_id=obj.company_id
+                )
+            else:
+                kwargs["queryset"] = AccessRole.objects.filter(
+                    company_id__exact=None
+                )
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_form(self, request, obj=None, **kwargs):
+        # Store the object being edited in the request
+        request._obj_ = obj
+        return super().get_form(request, obj, **kwargs)
 
     def has_change_permission(self, request, obj=None):
         # Check if the user has permission to change the object
@@ -37,7 +80,7 @@ class InvitationAdmin(admin.ModelAdmin):
         if user.is_super_user:
             return True
         else:
-            return module_perm("user", user, "update")
+            return module_perm("invitation", user, "update")
 
     def has_view_permission(self, request, obj=None):
         # Check if the user has permission to change the object
@@ -45,14 +88,14 @@ class InvitationAdmin(admin.ModelAdmin):
         if user.is_super_user:
             return True
         else:
-            return module_perm("user", user, "view")
+            return module_perm("invitation", user, "view")
 
     def has_add_permission(self, request):
         user = request.user
         if user.is_super_user:
             return True
         else:
-            return module_perm("user", user, "add")
+            return module_perm("invitation", user, "add")
 
     def has_delete_permission(self, request, obj=None):
         # Check if the user has permission to delete the object
@@ -60,7 +103,7 @@ class InvitationAdmin(admin.ModelAdmin):
         if user.is_super_user:
             return True
         else:
-            return module_perm("user", user, "delete")
+            return module_perm("invitation", user, "delete")
 
 
 class InvitationSpecificAdmin(admin.ModelAdmin):
@@ -69,6 +112,28 @@ class InvitationSpecificAdmin(admin.ModelAdmin):
     search_fields = ("fullname", "email")
     readonly_fields = ("invite_link",)
     fields = ("fullname", "email", "role", "invite_link", "is_active")
+
+    def regenerate_invitation(self, request, queryset):
+        for invitation in queryset:
+            # Generate a new invite_link and send it to the email
+            user_obj = User.objects.filter(email=invitation.email)
+            if user_obj:
+                user_obj.delete()
+            invitation.is_active = True
+            invitation.save()
+            # Send the new invite to the email (you should implement this part)
+
+    regenerate_invitation.short_description = "Regenerate Invitations"
+
+    actions = [regenerate_invitation]
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        user = request.user
+        if not user.is_company_owner:
+            if not module_perm("invitation", user, "regenerate"):
+                actions.pop("regenerate_invitation", None)
+        return actions
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "role":
