6d0f88a1a84f8b6540c939582a0f1d3bf71ba8a7
rishabh dewangan <rishabh@nickelfox.com>
2023-09-21 19:57:05 +0530

    feat(role admin): Role Admin Changes

--- apps/role/admin.py
+++ apps/role/admin.py
@@ -31,6 +31,15 @@ class AccessRoleAdmin(admin.ModelAdmin):
     form = AccessRoleAdminForm
     list_display = ["name", "id", "role_permissions"]
 
+    def save_model(self, request, obj, form, change):
+        # Save the object initially to generate obj.id
+        super().save_model(request, obj, form, change)
+
+        # Check if this is a new invitation being added (not an update)
+        if not change:
+            obj.company_id = None
+            obj.save()
+
     def has_change_permission(self, request, obj=None):
         # Check if the user has permission to change the object
         user = request.user
