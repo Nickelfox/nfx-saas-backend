1bfccec374108246011e372a442f84b0c2ce7058
rishabh-nickelfox <rishabh@nickelfox.com>
2023-11-08 15:39:20 +0530

    feat(team apis): Added team apis

--- apps/team/views.py
+++ apps/team/views.py
@@ -1,3 +1,193 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, permissions, filters
+from rest_framework.response import Response
+from .models import Team
+from .serializers import TeamSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from common.helpers import module_perm
+from common.constants import ApplicationMessages
 
-# Create your views here.
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]  # Add DjangoFilterBackend
+    filterset_fields = [
+        "user",
+        "department",
+    ]  # Define the fields available for filtering
+
+    # Define fields available for filtering and ordering
+    ordering_fields = [
+        "capacity",
+        "work_days",
+        "department",
+    ]
+    search_fields = [
+        "user",
+        "department",
+    ]
+
+    def get_queryset(self):
+        # Filter projects by the user's company
+        user = self.request.user
+        return Team.objects.filter(company_id=user.company_id)
+
+    def list(self, request):
+        # Check if the user has permission to list projects
+        req_user = request.user
+        if req_user.is_company_owner or module_perm("team", req_user, "view"):
+            queryset = self.filter_queryset(self.get_queryset())
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(
+                {
+                    "status": ApplicationMessages.SUCCESS,
+                    "data": serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {
+                    "status": "error",
+                    "message": ApplicationMessages.PERMISSION_DENIED,
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+    def create(self, request):
+        # Check if the user has permission to add a project
+        req_user = request.user
+        if req_user.is_company_owner or module_perm("team", req_user, "add"):
+            # Get the user's company_id from the request user
+            company_id = req_user.company.id
+            # Serialize the data
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+
+            # Update the 'company_id' field
+            instance = serializer.instance
+            instance.company_id = (
+                request.user.company_id
+            )  # Assuming 'client' is a ForeignKey
+            instance.save()
+
+            return Response(
+                {
+                    "status": ApplicationMessages.SUCCESS,
+                    "data": serializer.data,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        else:
+            return Response(
+                {
+                    "status": "error",
+                    "message": ApplicationMessages.PERMISSION_DENIED,
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+    def retrieve(self, request, pk=None):
+        instance = self.get_object()
+        # Check if the user has permission to retrieve this project
+        req_user = request.user
+        if req_user.is_company_owner or module_perm("team", req_user, "view"):
+            serializer = self.get_serializer(instance)
+            return Response(
+                {
+                    "status": ApplicationMessages.SUCCESS,
+                    "data": serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {
+                    "status": "error",
+                    "message": ApplicationMessages.PERMISSION_DENIED,
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+    def update(self, request, pk=None):
+        instance = self.get_object()
+        # Check if the user has permission to update this project
+        req_user = request.user
+        if req_user.is_company_owner or module_perm(
+            "team", req_user, "update"
+        ):
+            serializer = self.get_serializer(instance, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            return Response(
+                {
+                    "status": ApplicationMessages.SUCCESS,
+                    "data": serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {
+                    "status": "error",
+                    "message": ApplicationMessages.PERMISSION_DENIED,
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+    def partial_update(self, request, pk=None):
+        instance = self.get_object()
+        # Check if the user has permission to partially update this project
+        req_user = request.user
+        if req_user.is_company_owner or module_perm(
+            "team", req_user, "update"
+        ):
+            serializer = self.get_serializer(
+                instance, data=request.data, partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            return Response(
+                {
+                    "status": ApplicationMessages.SUCCESS,
+                    "data": serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {
+                    "status": "error",
+                    "message": ApplicationMessages.PERMISSION_DENIED,
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+    def destroy(self, request, pk=None):
+        instance = self.get_object()
+        # Check if the user has permission to delete this project
+        req_user = request.user
+        if req_user.is_company_owner or module_perm(
+            "team", req_user, "delete"
+        ):
+            self.perform_destroy(instance)
+            return Response(
+                {
+                    "status": ApplicationMessages.SUCCESS,
+                    "message": ApplicationMessages.DELETED_SUCCESS,
+                },
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        else:
+            return Response(
+                {
+                    "status": "error",
+                    "message": ApplicationMessages.PERMISSION_DENIED,
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
