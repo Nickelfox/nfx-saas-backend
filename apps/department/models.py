b0f7563ee165a036ba2304b6c7dc0a8f4a6eb3a6
rishabh-nickelfox <rishabh@nickelfox.com>
2023-11-30 10:45:13 +0530

    fix(deparment model changes): added unique together constraint

--- apps/department/models.py
+++ apps/department/models.py
@@ -10,12 +10,13 @@ class Department(BaseModel):
     Department model is to specify the name of departments
     """
 
-    name = models.CharField(
-        max_length=255, null=False, blank=False, unique=True
-    )
+    name = models.CharField(max_length=255, null=False, blank=False)
     company = models.ForeignKey(
         Company, on_delete=models.PROTECT, null=True, blank=True
     )
+
+    class Meta:
+        unique_together = ["name", "company"]
 
     def __str__(self):
         """
