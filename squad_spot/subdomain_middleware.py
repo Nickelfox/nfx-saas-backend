432f021a1b0968b56417adbd50773e5deec423f9
rishabh-nickelfox <rishabh@nickelfox.com>
2023-12-07 14:45:38 +0530

    refactor(uncommented code): UncommentedSubdomain logic changes

--- squad_spot/subdomain_middleware.py
+++ squad_spot/subdomain_middleware.py
@@ -7,6 +7,7 @@ from common.constants import (
     COMPANY_ADMIN_ROUTE_NAME,
     ApplicationMessages,
 )
+from django.conf import settings
 
 
 class CustomLoginRedirectMiddleware:
@@ -15,70 +16,47 @@ class CustomLoginRedirectMiddleware:
 
     def __call__(self, request):
         # Your custom logic to determine the redirect URL here
-        # host = request.get_host()
+        host = request.get_host()
+        base_url = settings.HOST_URL.lower().replace("https://", "")
+        base_url = base_url.replace("http://", "")
+        host = host.replace(base_url, "")
+        company = host.replace(".", "")
         # parts = host.split(".")
-        # if len(parts) == 2:
-        #     company = parts[0]
-        #     if company:
-        #         if f"{SQUAD_SPOT_ADMIN_ROUTE_NAME}" in request.path:
-        #             return render(
-        #                 request,
-        #                 "unauthorized_access.html",
-        #                 {"error_message": ApplicationMessages.COMPANY_INVALID},
-        #                 status=401,
-        #             )
+        if company:
+            if f"{SQUAD_SPOT_ADMIN_ROUTE_NAME}" in request.path:
+                return render(
+                    request,
+                    "unauthorized_access.html",
+                    {"error_message": ApplicationMessages.COMPANY_INVALID},
+                    status=401,
+                )
 
-        #     # Validate company
-        #     valid_company = Company.objects.filter(
-        #         name__iexact=company.replace("-", " ").lower()
-        #     ).exists()
-        #     if not valid_company:
-        #         return render(request, "invalid_route.html", status=404)
+            # Validate company
+            valid_company = Company.objects.filter(
+                name__iexact=company.replace("-", " ").lower()
+            ).exists()
+            if not valid_company:
+                return render(request, "invalid_route.html", status=404)
 
-        # else:
-        #     company = None
-        #     if (
-        #         f"{SQUAD_SPOT_ADMIN_ROUTE_NAME}" not in request.path
-        #         and f"{COMPANY_ADMIN_ROUTE_NAME}" in request.path
-        #     ):
-        #         return render(
-        #             request,
-        #             "unauthorized_access.html",
-        #             {"error_message": ApplicationMessages.COMPANY_INVALID},
-        #             status=401,
-        #         )
+        else:
+            company = None
+            if (
+                f"{SQUAD_SPOT_ADMIN_ROUTE_NAME}" not in request.path
+                and f"{COMPANY_ADMIN_ROUTE_NAME}" in request.path
+            ):
+                return render(
+                    request,
+                    "unauthorized_access.html",
+                    {"error_message": ApplicationMessages.COMPANY_INVALID},
+                    status=401,
+                )
 
-        # if request.user.is_authenticated:
-        #     if (
-        #         company
-        #         and request.user.company.name.replace(" ", "-").lower()
-        #         != company
-        #     ):
-        #         return render(request, "invalid_route.html", status=404)
-        # response = self.get_response(request)
-        # return response
-
-        user = request.user
-        if user.is_authenticated:
-            if user.company:
-                if f"{SQUAD_SPOT_ADMIN_ROUTE_NAME}" in request.path:
-                    return render(
-                        request,
-                        "unauthorized_access.html",
-                        {"error_message": ApplicationMessages.COMPANY_INVALID},
-                        status=401,
-                    )
-            else:
-                if (
-                    f"{SQUAD_SPOT_ADMIN_ROUTE_NAME}" not in request.path
-                    and f"{COMPANY_ADMIN_ROUTE_NAME}" in request.path
-                ):
-                    return render(
-                        request,
-                        "unauthorized_access.html",
-                        {"error_message": ApplicationMessages.COMPANY_INVALID},
-                        status=401,
-                    )
-
+        if request.user.is_authenticated:
+            if (
+                company
+                and request.user.company.name.replace(" ", "-").lower()
+                != company
+            ):
+                return render(request, "invalid_route.html", status=404)
         response = self.get_response(request)
         return response
