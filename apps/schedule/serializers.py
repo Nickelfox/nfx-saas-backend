aae834f79e9ca4c58c3ac02fee284678f675fe27
rishabh-nickelfox <rishabh@nickelfox.com>
2023-12-07 20:33:13 +0530

    feat(member_id added): Added member_id in response

--- apps/schedule/serializers.py
+++ apps/schedule/serializers.py
@@ -18,7 +18,10 @@ class ScheduleSerializer(serializers.ModelSerializer):
 
 
 class SchedulelistSerializer(serializers.ModelSerializer):
-    project_id = serializers.CharField(source="project_member.project_id")
+    project_id = serializers.UUIDField(source="project_member.project_id")
+    member_id = serializers.UUIDField(source="project_member.member_id")
+    assigned_hours = serializers.SerializerMethodField(source="assigned_hour")
+
     department_id = serializers.UUIDField(
         source="project_member.member.department_id"
     )
@@ -32,11 +35,19 @@ class SchedulelistSerializer(serializers.ModelSerializer):
             "id",
             "project_member",
             "project_id",
+            "member_id",
             "department_id",
             "department_name",
             "start_at",
             "end_at",
             "notes",
-            "assigned_hour",
+            "assigned_hours",
             "schedule_type",
         ]
+
+    def get_assigned_hours(self, obj):
+        return (
+            int(obj.assigned_hour.total_seconds())
+            if obj.assigned_hour
+            else None
+        )
