9ed5bcc4ab4e05643e424e6792afa29c135f0b22
rishabh-nickelfox <rishabh@nickelfox.com>
2023-12-10 17:55:53 +0530

    feat(schedule delete): Updated Schedule delete response

--- apps/schedule/views.py
+++ apps/schedule/views.py
@@ -218,11 +218,11 @@ class ScheduleViewSet(viewsets.ModelViewSet):
             self.perform_destroy(instance)
             return Response(
                 {
-                    "status": status.HTTP_204_NO_CONTENT,
-                    "message": ApplicationMessages.SUCCESS,
+                    "status": status.HTTP_200_OK,
+                    "message": ApplicationMessages.DELETED_SUCCESS,
                     "data": {},
                 },
-                status=status.HTTP_204_NO_CONTENT,
+                status=status.HTTP_200_OK,
             )
         else:
             return Response(
