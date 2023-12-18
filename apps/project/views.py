0b42b256bd8aa496fb71503e8a948ccd0d63935c
rishabh-nickelfox <rishabh@nickelfox.com>
2023-12-18 20:06:30 +0530

    refactor(project member multiple create): Updated project memeber create method and serializer

--- apps/project/views.py
+++ apps/project/views.py
@@ -177,15 +177,9 @@ class ProjectMemberViewSet(viewsets.ModelViewSet):
         )
 
     def create(self, request):
-        existing_instance = ProjectMember.objects.filter(
-            project=request.data["project"], member=request.data["member"]
-        ).first()
-        if existing_instance:
-            serializer = self.get_serializer(existing_instance)
-        else:
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
 
         return Response(
             {
